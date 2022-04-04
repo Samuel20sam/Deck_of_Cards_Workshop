f033e8f172d390bdc2106130f92c540b7c8aeb0c
Samuel_Elijah <samuel20sam@gmail.com>
2022-04-04 21:05:39 +0530

    Adding: Creating players and adding to console

--- src/com/bridgelabz/deckofcards/Name.java
+++ src/com/bridgelabz/deckofcards/Name.java
@@ -3,17 +3,12 @@ package com.bridgelabz.deckofcards;
 public class Name
 {
     String name;
-    String[] cards;
 
-    public Name (String name, int noOfCards) {
+    public Name (String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String[] getCards() {
-        return cards;
     }
 }
