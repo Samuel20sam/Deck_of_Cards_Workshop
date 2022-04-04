d38cc73de2dffde818392f624ba1cf2b4073fa92
Samuel_Elijah <samuel20sam@gmail.com>
2022-04-04 21:00:23 +0530

    Adding: Creating players and adding to console

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -8,19 +8,24 @@ public class DeckOfCards
         String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
         String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         String[][] deck = new String[4][13];
-        for (int i = 0; i < suit.length; i++) {
-            for (int j = 0; j < rank.length; j++) {
+        for (int i = 0; i < suit.length; i++)
+        {
+            for (int j = 0; j < rank.length; j++)
+            {
                 deck[i][j] = rank[j] + "(" + suit[i] + ")";
             }
         }
         String[][] shuffledDeck = shuffle(suit, rank, deck);
         System.out.println(Arrays.deepToString(shuffledDeck));
+        new Player(4, 9, shuffledDeck);
     }
 
     private static String[][] shuffle(String[] suit, String[] rank, String[][] deck) {
         String[] temp = new String[1];
-        for (int i = 0; i < suit.length; i++) {
-            for (int j = 0; j < rank.length; j++) {
+        for (int i = 0; i < suit.length; i++)
+        {
+            for (int j = 0; j < rank.length; j++)
+            {
                 int random1 = (int) Math.floor(Math.random() * 10) % 4;
                 int random2 = (int) Math.floor(Math.random() * 100) % 13;
                 temp[0] = deck[i][j];
