6e3251e590b026cf3068f8b88f51b8bb091e3bc3
Samuel_Elijah <samuel20sam@gmail.com>
2022-04-06 19:03:41 +0530

    Adding: method to sequence the Players order

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,39 +1,32 @@
 package com.bridgelabz.deckofcards;
 
-import java.util.Arrays;
-
-public class DeckOfCards
+public class DeckOfCards extends Players
 {
-    public static void main(String[] args) {
-        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
-        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-        String[][] deck = new String[4][13];
-        for (int i = 0; i < suit.length; i++)
-        {
-            for (int j = 0; j < rank.length; j++)
-            {
-                deck[i][j] = rank[j] + "(" + suit[i] + ")";
+    public String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+    public String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    int n = suits.length * ranks.length;
+
+    public void shuffle()
+    {
+        String[] deck = new String[this.n];
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                deck[suits.length * i + j] = ranks[i] + " - " + suits[j];
             }
         }
-        String[][] shuffledDeck = shuffle(suit, rank, deck);
-        System.out.println(Arrays.deepToString(shuffledDeck));
-        new Player(4, 13, shuffledDeck);
+        for (int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n - i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
     }
 
-    private static String[][] shuffle(String[] suit, String[] rank, String[][] deck) {
-        String[] temp = new String[1];
-        for (int i = 0; i < suit.length; i++)
-        {
-            for (int j = 0; j < rank.length; j++)
-            {
-                int random1 = (int) Math.floor(Math.random() * 10) % 4;
-                int random2 = (int) Math.floor(Math.random() * 100) % 13;
-                temp[0] = deck[i][j];
-                deck[i][j] = deck[random1][random2];
-                deck[random1][random2] = temp[0];
-            }
-
-        }
-        return deck;
+    public static void main(String[] args)
+    {
+        DeckOfCards deckOfCards = new DeckOfCards();
+        deckOfCards.shuffle();
+        deckOfCards.Player();
+        deckOfCards.order();
     }
 }
