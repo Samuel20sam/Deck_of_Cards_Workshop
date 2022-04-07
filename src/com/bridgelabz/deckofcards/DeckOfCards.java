8ddf75554351ac5752ea92ccd678f2a804734ddd
Samuel_Elijah <samuel20sam@gmail.com>
2022-04-07 17:48:01 +0530

    Adding: shuffle feature

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,34 +1,43 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.Arrays;
+
 public class DeckOfCards extends Players
 {
-    public String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-    public String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-    int n = suits.length * ranks.length;
+    public static String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+    public static String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    public static int n = suits.length * ranks.length;
 
-    public void shuffle()
+    public String[] start()
     {
-        String[] deck = new String[this.n];
+        String[] deck = new String[n];
         for (int i = 0; i < ranks.length; i++) {
             for (int j = 0; j < suits.length; j++) {
                 deck[suits.length * i + j] = ranks[i] + " - " + suits[j];
-
             }
         }
+    return deck;
+    }
+
+    public static String[] shuffle(String[] deck)
+    {
         for (int i = 0; i < n; i++) {
             int r = i + (int) (Math.random() * (n - i));
             String temp = deck[r];
             deck[r] = deck[i];
             deck[i] = temp;
         }
+    return deck;
     }
 
     public static void main(String[] args)
     {
         DeckOfCards deckOfCards = new DeckOfCards();
-        deckOfCards.shuffle();
-        deckOfCards.Player();
-        deckOfCards.order();
-
+        Players players = new Players();
+        players.Player();
+        players.order();
+        String[] deck = deckOfCards.start();
+        String[] deckShuffle = shuffle(deck);
+        System.out.println(Arrays.toString(deckShuffle));
     }
 }
--- src/com/bridgelabz/deckofcards/Players.java
+++ src/com/bridgelabz/deckofcards/Players.java
@@ -5,39 +5,32 @@ import java.util.Scanner;
 
 public class Players
 {
-    public String[] playersName = new String[4];
+        public String[] playersName = new String[4];
 
     public void Player() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of player");
-        int p = scan.nextInt();
-        if (p > 4 || p < 2) {
+        int pn = scan.nextInt();
+        if (pn > 4 || pn < 2) {
             System.out.println("Enter player from 2 to 4");
             Player();
         }
-        for (int i = 0; i < p; i++) {
+        for (int i = 0; i < pn; i++) {
             Scanner scan1 = new Scanner(System.in);
-            System.out.println("Enter the player " + (i + 1) + " name :");
+            System.out.println("Player " + (i + 1) + " : Enter the name of the player");
             String name = scan1.nextLine();
             playersName[i] = name;
         }
     }
 
     public void order() {
-        for (int i = 0; i < playersName.length; i++) {
-            if (playersName[i] == null) {
-                break;
-            }
-            for (int j = 0; j < playersName.length; j++) {
-                if (playersName[j] == null) {
-                    continue;
-                }
-                if (playersName[i].hashCode() != Arrays.hashCode(playersName)) {
-                    if (playersName[i].length() > playersName[j].length()) {
-                        String temp = playersName[j];
-                        playersName[j] = playersName[i];
-                        playersName[i] = temp;
-                    }
+        for (int i = 0; i < playersName.length - 1; i++) {
+            for (int j = 0; j < playersName.length - i - 1; j++) {
+                if (playersName[j].length() > playersName[j+1].length()) {
+                    String temp = playersName[j+1];
+                    playersName[j] = playersName[j+1];
+                    playersName[j+1] = temp;
+                    System.out.println(Arrays.toString(playersName));
                 }
             }
         }
